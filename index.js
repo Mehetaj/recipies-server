9f54929cf75a494b64747e7d5e3bfe6b7982624b
Mehetaj <121656128+Mehetaj@users.noreply.github.com>
2023-05-01 22:54:32 +0600

    all data send

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 5000;
+const cors = require('cors')
+
+app.use(cors())
+
+
+const chefs = require('./chefs.json')
+
+app.get('/', (req , res) => {
+    res.send("Assignment 10 server running")
+})
+
+app.get('/chefs', (req ,res) => {
+    res.send(chefs)
+})
 
 
 app.listen(port, () => {
